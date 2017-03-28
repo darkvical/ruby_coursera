6ba29c1628b397c2d9b685e4f3b8be27c8319ce7
Victor Rodriguez <vical.rl@gmail.com>
2017-03-28 00:58:01 -0300

    Adicion de ejemplo rest

--- ejemplos/restHelper/Coursera.rb
+++ ejemplos/restHelper/Coursera.rb
@@ -13,6 +13,6 @@ class Coursera
 	end
 end
 
-pp Coursera.for "ruby"
+pp Coursera.for "python"
 #pp Coursera.getRecipe("7c89c610b36bc55175e2df8c176f175c", "shredded chicken")
 #puts Coursera.for "python"
